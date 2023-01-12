e8a3b298f93e381131ca75efc2f94db577c16d98
Alexander Smolyakov <alexander.smolyakov.dev@gmail.com>
2023-01-12 19:33:16 +0400

    Add `TOXENV` env variable

--- scripts/env-setup.sh
+++ scripts/env-setup.sh
@@ -3,3 +3,4 @@ echo "DBT_TEST_USER_1=dbt_test_role_1" >> $GITHUB_ENV
 echo "DBT_TEST_USER_2=dbt_test_role_2" >> $GITHUB_ENV
 echo "DBT_TEST_USER_3=dbt_test_role_3" >> $GITHUB_ENV
 echo "INTEGRATION_TESTS_SECRETS_PREFIX=SNOWFLAKE_TEST" >> $GITHUB_ENV
+echo "TOXENV=integration-snowflake" >> $GITHUB_ENV
